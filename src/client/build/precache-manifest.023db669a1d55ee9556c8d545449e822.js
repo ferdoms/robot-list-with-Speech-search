8a89a15ea532d7b5ea5d00e1038eb82051b12b66
ferdoms <marinhosilva.fernando@gmail.com>
2019-07-24 12:58:05 +0100

    build updated

--- src/client/build/precache-manifest.023db669a1d55ee9556c8d545449e822.js
+++ src/client/build/precache-manifest.023db669a1d55ee9556c8d545449e822.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f635043db3d16584b285227ba622e0d8",
+    "revision": "8ef8802e1a6bc27c72728726ee1a536d",
     "url": "/index.html"
   },
   {
-    "revision": "0c5c734b561f24f5ff15",
+    "revision": "eb6d2b5667f577113bfd",
     "url": "/static/css/2.31dafeb2.chunk.css"
   },
   {
-    "revision": "725ca2812d0726af4114",
+    "revision": "01d50fdb0452b80ca9e2",
     "url": "/static/css/main.162a4228.chunk.css"
   },
   {
-    "revision": "0c5c734b561f24f5ff15",
-    "url": "/static/js/2.1daeec5d.chunk.js"
+    "revision": "eb6d2b5667f577113bfd",
+    "url": "/static/js/2.afb4cc6f.chunk.js"
   },
   {
-    "revision": "725ca2812d0726af4114",
-    "url": "/static/js/main.7da3cef8.chunk.js"
+    "revision": "01d50fdb0452b80ca9e2",
+    "url": "/static/js/main.2d9fd0eb.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
